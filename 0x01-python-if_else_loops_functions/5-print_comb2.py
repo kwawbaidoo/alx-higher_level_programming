3e3bb8e6077c71b70a738db57982ba995f8fee0e
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-06-29 09:27:38 -0700

    python 0x01

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-	if i == 99:
-        	print("{}".format(i))
+for num in range(0, 100):
+	if num == 99:
+		print("{}".format(num))
 	else:
-        	print("{:02}".format(i), end=",")
+		print("{:02}".format(num), end=", ")
