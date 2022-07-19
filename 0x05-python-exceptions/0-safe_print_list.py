792dc9e1bba06e67cf52ddb38d72a8c8486a3369
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-07-18 21:34:15 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,13 @@
 
 
 def safe_print_list(my_list=[], x=0):
-	i = 0
-	printed = 0
-	for i in range(0, x):
-		try:
-			print("{:d}".format(my_list[i], end="")
-			print += 1
-		except:
-			continue
-	print()
-	return printed
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
+    print()
+    return printed
