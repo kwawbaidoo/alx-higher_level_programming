f7c199dd89f7a3cb0921eac946c4ec328d3800b4
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-10-05 16:41:31 +0000

    Javascript is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Print string "Javascript is amazing"
+// Print string “Javascript is amazing”
 
-const myvar = 'JavaScript is amazing';
-console.log(myvar);
+const myVar = 'Javascript is amazing';
+console.log(myVar);
