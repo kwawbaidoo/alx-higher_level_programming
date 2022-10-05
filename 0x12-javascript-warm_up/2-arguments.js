4db5f02c28f67cc397422e9188a3f727864ffd47
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-10-05 19:37:17 +0000

    Argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const arg = process.argv.argument;
 
 if (arg < 3) {
-	console.log('No argument');
-} else if (arg === 3){
-	console.log('Argument found');
+  console.log('No argument');
+} else if (arg === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Argument found');
+  console.log('Argument found');
 }
