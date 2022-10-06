0a4d3878aace39926979dd126fc7d4d11568fcb4
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-10-06 05:38:27 +0000

    Sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// script that imports an array and computes a new array.
+// function that returns the number of occurrences in a list
 
 const originalList = require('./100-data').list;
 console.log(originalList);
 const mappedList = originalList.map (function (e, index) {
-    return (e * index);
+  return (e * index);
 });
 console.log(mappedList);
