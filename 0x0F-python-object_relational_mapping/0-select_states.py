6f5ef92bd8fe143975f14d7ada508dcbce7e61ee
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-10-10 00:56:19 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-
-    for row in rows:
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    for row in cur.fetchall():
         print(row)
+
+	db.close()
