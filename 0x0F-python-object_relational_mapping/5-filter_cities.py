8a58096773537aa19886db9e57603815fb7d63a0
kwawbaidoo <kwaw.baidoo@gmail.com>
2022-10-10 02:39:07 -0700

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
+"""Exercise
 """
-python script that lists all cities from the database hbtn_0e_4_usa
-with specified state name
-"""
-
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities \
-    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-    ORDER BY cities.id", (argv[4],))
-    rows = cursor.fetchall()
-    print(", ".join(city[0] for city in rows))
-    cursor.close()
+
+def filter_city():
+    """filter cities
+    """
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states \
+                ON cities.state_id=states.id \
+                WHERE states.name=%s ORDER BY cities.id ASC", (argv[4],))
+    query_rows = cur.fetchall()
+
+    list_data = []
+    for row in query_rows:
+        list_data.append(row[0])
+
+    print(', '.join(list_data))
+
     db.close()
+
+
+if __name__ == '__main__':
+    filter_city()
